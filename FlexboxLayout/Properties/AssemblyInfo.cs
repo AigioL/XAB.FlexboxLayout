fd6865625705755a7db943aa2ad987bff723d696
Aigio Liu <lysoft.pw@foxmail.com>
2018-10-24 21:11:15 +0800

    v1.0

---
+++ FlexboxLayout/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+[assembly: SuppressIldasm]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FlexboxLayout")]
+[assembly: AssemblyDescription("https://bintray.com/google/flexbox-layout/flexbox/1.0.0")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("FlexboxLayout")]
+//[assembly: AssemblyCopyright("Copyright ©  2018")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+
+// https://bintray.com/google/flexbox-layout/flexbox/1.0.0
+// https://github.com/google/flexbox-layout/tree/1.0.0
